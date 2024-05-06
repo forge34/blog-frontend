29390e3ab899eb0729f5cc41d6f5ee7f473e246b
Forge <>
2024-05-07 00:44:18 +0300

    test : add test for login form component

--- src/components/login-form.jsx
+++ src/components/login-form.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { TextInput } from "./form-controls/text-input";
+import { useNavigate } from "react-router-dom";
 
 function LoginForm() {
-  function handleSubmit(e) {}
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    navigate("/");
+  }
 
   return (
-    <>
+    <div>
+      <h3> Login form</h3>
       <form onSubmit={handleSubmit}>
         <TextInput name={"username"} label={"username"}></TextInput>
         <TextInput name={"password"} label={"password"}></TextInput>
         <button type="submit">Login</button>
       </form>
-    </>
+    </div>
   );
 }
-
-LoginForm.propTypes = {};
 
 export { LoginForm };
---
+++ src/tests/login-form.test.jsx
@@ -0,0 +1,40 @@
+import { render, screen } from "@testing-library/react";
+import { LoginForm } from "../components/login-form";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from "react-router-dom";
+
+describe("Login form test", () => {
+  it("should match snapshot", () => {
+    const { form } = render(
+      <MemoryRouter>
+        <LoginForm></LoginForm>
+      </MemoryRouter>,
+    );
+
+    expect(form).toMatchSnapshot();
+  });
+
+  it("should render submit button", () => {
+    render(
+      <MemoryRouter>
+        <LoginForm></LoginForm>
+      </MemoryRouter>,
+    );
+
+    expect(screen.getByRole("button")).toHaveTextContent("Login");
+  });
+
+  it("should navigate on submit", async () => {
+    render(
+      <MemoryRouter>
+        <LoginForm></LoginForm>
+      </MemoryRouter>,
+    );
+
+    const user = userEvent.setup();
+
+    await user.click(screen.getByRole("button"));
+
+    expect(location.pathname).toMatch("/");
+  });
+});
