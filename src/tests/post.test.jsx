65b9279513704dc9eb1e7e039dfc74f33a0375ac
Forge <>
2024-05-09 16:01:37 +0300

    test : add test for signup form & post components

---
+++ src/tests/post.test.jsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import Post from "../components/blog/post";
+
+describe("Post component test", () => {
+  const renderPost = () => {
+    return render(
+      <Post
+        title="First post"
+        author="forge"
+        date="09/05/2024"
+        body="Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis."
+      ></Post>,
+    );
+  };
+
+  it("should match snapshot", () => {
+    const { post } = renderPost();
+
+    expect(post).toMatchSnapshot();
+  });
+
+  it("should render title correctly", () => {
+    renderPost();
+    screen.debug();
+    expect(screen.getByRole("heading").textContent).toMatch("First post");
+  });
+});
--- src/tests/signup-form.test.jsx
+++ src/tests/signup-form.test.jsx
@@ -1,16 +1,15 @@
 import { render } from "@testing-library/react";
-import SignupForm from "../components/signup-form";
-import userEvent from "@testing-library/user-event";
+import SignupForm from "../components/authentication/signup-form";
 import { MemoryRouter } from "react-router-dom";
 
 describe("Signup form test", () => {
   it("should match snapshot", () => {
-    const {form} = render(
+    const { form } = render(
       <MemoryRouter>
         <SignupForm></SignupForm>
       </MemoryRouter>,
     );
 
-    expect(form).toMatchSnapshot()
+    expect(form).toMatchSnapshot();
   });
 });
