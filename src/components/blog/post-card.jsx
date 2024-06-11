1145b0f27d10dc2e7f7f9dbd7ec27523d2d222c8
Forge <>
2024-06-11 15:07:18 +0300

    feat : add homepage css style

--- src/components/blog/post-card.jsx
+++ src/components/blog/post-card.jsx
@@ -1,21 +1,22 @@
-
 import React from "react";
 import PropTypes from "prop-types";
+import styles from "../../styles/sass/post-card.module.scss";
 
-function PostCard({ title, body, author, date }) {
+function PostCard({ title, body, author, date = "2/4/2024" }) {
   return (
-    <div>
-      <div>
-        <img></img>
-      </div>
-      <div>
-        <h2>{title}</h2>
-
-        <div>
+    <div className={styles.container}>
+      <img src={"https://placehold.co/600x400"}></img>
+      <div className={styles.cardContent}>
+        <div className={styles.cardInfo}>
           <p>{author}</p>
           <p>{date}</p>
         </div>
-        <p>{body}</p>
+        <h2>{title}</h2>
+
+        <p className={styles.cardText}>
+          {body.split(" ").length > 150 ? body.substring(0, 150) : body}
+        </p>
+        <button className={styles.cardBtn}> Read post </button>
       </div>
     </div>
   );
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,37 +1,43 @@
 import Header from "../components/blog/header";
 import { Link, Outlet, useLocation } from "react-router-dom";
-import PostCard from "../components/blog/post-card"
+import PostCard from "../components/blog/post-card";
 import { useLoaderData } from "react-router-dom";
+import styles from "../styles/css/home.module.css";
 
 export default function Home() {
   const location = useLocation();
-  const data = useLoaderData()
+  const data = useLoaderData();
 
-
-  return (<>
-    <Header ></Header>
-    <div>
-      {location.pathname === "/home" ? (
-        <>
-          <Link to="posts" relative="path">
-            See all posts
-          </Link>
-          {data.map((e) => {
-            return (
-              <PostCard
-                title={e.title}
-                id={e._id}
-                author={e.author.username}
-                key={e._id}
-                body={e.body}
-              ></PostCard>
-            );
-          })}
-
-        </>
-      ) : (
-        <Outlet></Outlet>
-      )}
-    </div></>
+  return (
+    <>
+      <Header></Header>
+      <div className={styles.container}>
+        {location.pathname === "/home" ? (
+          <>
+            <Link to="posts" className={styles.title} relative="path">
+              See all posts
+            </Link>
+            <div className={styles.cardContainer}>
+              {data.map((e, i) => {
+                return (
+                  i < 3 && (
+                    <PostCard
+                      title={e.title}
+                      id={e._id}
+                      author={e.author.username}
+                      key={e._id}
+                      body={e.body}
+                      date={e.date}
+                    ></PostCard>
+                  )
+                );
+              })}
+            </div>
+          </>
+        ) : (
+          <Outlet></Outlet>
+        )}
+      </div>
+    </>
   );
 }
