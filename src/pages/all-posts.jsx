2b521a2df2e51330ae6c3acf234449908ea8b933
forge34 <28455546+forge34@users.noreply.github.com>
2024-07-16 01:23:55 +0300

    apply fix to all-posts component

--- src/pages/all-posts.jsx
+++ src/pages/all-posts.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { useLoaderData } from "react-router-dom";
+import React, { Suspense } from "react";
+import { Await, useLoaderData } from "react-router-dom";
 import PostLink from "../components/blog/post-link";
-
+import { InfinitySpin } from "react-loader-spinner";
 function AllPosts() {
   const data = useLoaderData();
 
@@ -15,16 +15,31 @@ function AllPosts() {
         flexWrap: "wrap",
       }}
     >
-      {data.map((e) => {
-        return (
-          <PostLink
-            title={e.title}
-            id={e._id}
-            author={e.author.username}
-            key={e._id}
-          ></PostLink>
-        );
-      })}
+      <Suspense
+        fallback={
+          <InfinitySpin
+            visible={true}
+            width="580"
+            color="#514ec1"
+            ariaLabel="infinity-spin-loading"
+          ></InfinitySpin>
+        }
+      >
+        <Await resolve={data.posts} errorElement={<p>Loading failed ....</p>}>
+          {(posts) =>
+            posts.map((e) => {
+              return (
+                <PostLink
+                  title={e.title}
+                  id={e._id}
+                  author={e.author.username}
+                  key={e._id}
+                ></PostLink>
+              );
+            })
+          }
+        </Await>
+      </Suspense>
     </div>
   );
 }
