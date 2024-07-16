6267174d2ee271e37cc35fdaea54668acd45dce2
forge34 <28455546+forge34@users.noreply.github.com>
2024-07-16 18:55:08 +0300

    fix : add try / catch to use-auth hook to catch errors

--- src/utils/use-auth.jsx
+++ src/utils/use-auth.jsx
@@ -2,23 +2,27 @@ import { useEffect, useState } from "react";
 
 function useAuth() {
   const [user, setUser] = useState({});
-  const [logged, setLogged] = useState(true);
+  const [logged, setLogged] = useState(false);
 
   useEffect(() => {
     async function check() {
-      let data = await fetch(
-        `${import.meta.env.VITE_API_URL}/api/users/verify`,
-        {
-          mode: "cors",
-          credentials: "include",
+      try {
+        let data = await fetch(
+          `${import.meta.env.VITE_API_URL}/api/users/verify`,
+          {
+            mode: "cors",
+            credentials: "include",
+          },
+        );
+        if (data.status === 401) {
+          setLogged(false);
+        } else if (data.status === 200) {
+          data = await data.json();
+          setUser(data.user);
+          setLogged(true);
         }
-      );
-      if (data.status === 401) {
-        setLogged(false);
-      } else if (data.status === 200) {
-        data = await data.json();
-        setUser(data.user);
-        setLogged(true);
+      } catch (e) {
+        console.log(e);
       }
     }
     check();
